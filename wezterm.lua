5564ba132a8f85667eccd1f6bcde4eab6854b91e
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-07-27 01:41:21 +0900

    edit configurations

--- wezterm.lua
+++ wezterm.lua
@@ -1,13 +1,13 @@
 local wezterm = require 'wezterm'
-local config = {}
 
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+
+config.window_decorations = "RESIZE"
 config.default_prog = { 'wsl', '~' }
-config.color_scheme = 'AdventureTime'
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.75
 config.window_close_confirmation = 'NeverPrompt'
 config.skip_close_confirmation_for_processes_named = {
   'bash',
@@ -29,14 +29,15 @@ config.font = wezterm.font('0xProto Nerd Font Mono', { weight = 'Regular', stret
 local act = wezterm.action
 config.keys = {
   -- new tab
-  { key = 't',         mods = 'SHIFT|CTRL', action = act.SpawnTab 'CurrentPaneDomain' },
+  { key = 't',          mods = 'SHIFT|CTRL', action = act.SpawnTab 'CurrentPaneDomain' },
   -- cursor movements
-  { key = 'LeftArrow', mods = 'SHIFT|CTRL', action = act.SendKey { key = 'b', mods = 'META', } },
-  { key = 'i',         mods = 'SHIFT|CTRL', action = act.SendKey { key = 'e', mods = 'CTRL', } },
-  { key = 'Space',     mods = 'CTRL',       action = act.ActivateCopyMode },
+  { key = 'LeftArrow',  mods = 'SHIFT|CTRL', action = act.SendKey { key = 'b', mods = 'META', } },
+  { key = 'RightArrow', mods = 'CTRL',       action = act.SendKey { key = 'f', mods = 'META', } },
+  { key = 'LeftArrow',  mods = 'CTRL',       action = act.SendKey { key = 'b', mods = 'META', } },
+  { key = 'i',          mods = 'SHIFT|CTRL', action = act.SendKey { key = 'e', mods = 'CTRL', } },
+  { key = 'Space',      mods = 'CTRL',       action = act.ActivateCopyMode },
   -- copy & paste
-  { key = 'c',         mods = 'CTRL',       action = act.CopyTo 'Clipboard' },
-  { key = 'v',         mods = 'CTRL',       action = act.PasteFrom 'Clipboard' },
+  { key = 'v',          mods = 'CTRL',       action = act.PasteFrom 'Clipboard' },
 }
 
 return config
