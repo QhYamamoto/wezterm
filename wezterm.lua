35229ddb718275ba8aa5da23c3447851e1201468
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-07-27 18:29:56 +0900

    standardize quote styles

--- wezterm.lua
+++ wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm'
+local wezterm = require "wezterm"
 
 if wezterm.config_builder then
   config = wezterm.config_builder()
@@ -6,26 +6,26 @@ end
 
 
 config.window_decorations = "RESIZE"
-config.default_prog = { 'wsl', '~' }
+config.default_prog = { "wsl", "~" }
 config.window_background_opacity = 0.75
-config.window_close_confirmation = 'NeverPrompt'
+config.window_close_confirmation = "NeverPrompt"
 config.audible_bell = "Disabled"
 config.skip_close_confirmation_for_processes_named = {
-  'bash',
-  'sh',
-  'zsh',
-  'fish',
-  'tmux',
-  'nu',
-  'cmd.exe',
-  'pwsh.exe',
-  'powershell.exe',
-  'wsl.exe',
-  'wslhost.exe',
-  'conhost.exe',
+  "bash",
+  "sh",
+  "zsh",
+  "fish",
+  "tmux",
+  "nu",
+  "cmd.exe",
+  "pwsh.exe",
+  "powershell.exe",
+  "wsl.exe",
+  "wslhost.exe",
+  "conhost.exe",
 }
 
-config.font = wezterm.font('0xProto Nerd Font Mono', { weight = 'Regular', stretch = 'Normal', style = 'Normal' })
+config.font = wezterm.font("0xProto Nerd Font Mono", { weight = "Regular", stretch = "Normal", style = "Normal" })
 
 local mux = wezterm.mux
 wezterm.on("gui-startup", function()
@@ -36,15 +36,15 @@ end)
 local act = wezterm.action
 config.keys = {
   -- new tab
-  { key = 't',          mods = 'SHIFT|CTRL', action = act.SpawnTab 'CurrentPaneDomain' },
+  { key = "t",          mods = "SHIFT|CTRL", action = act.SpawnTab "CurrentPaneDomain" },
   -- cursor movements
-  { key = 'LeftArrow',  mods = 'SHIFT|CTRL', action = act.SendKey { key = 'b', mods = 'META', } },
-  { key = 'RightArrow', mods = 'CTRL',       action = act.SendKey { key = 'f', mods = 'META', } },
-  { key = 'LeftArrow',  mods = 'CTRL',       action = act.SendKey { key = 'b', mods = 'META', } },
-  { key = 'i',          mods = 'SHIFT|CTRL', action = act.SendKey { key = 'e', mods = 'CTRL', } },
-  { key = 'Space',      mods = 'CTRL',       action = act.ActivateCopyMode },
+  { key = "LeftArrow",  mods = "SHIFT|CTRL", action = act.SendKey { key = "b", mods = "META", } },
+  { key = "RightArrow", mods = "CTRL",       action = act.SendKey { key = "f", mods = "META", } },
+  { key = "LeftArrow",  mods = "CTRL",       action = act.SendKey { key = "b", mods = "META", } },
+  { key = "i",          mods = "SHIFT|CTRL", action = act.SendKey { key = "e", mods = "CTRL", } },
   -- copy & paste
-  { key = 'v',          mods = 'CTRL',       action = act.PasteFrom 'Clipboard' },
+  { key = "Space",      mods = "CTRL",       action = act.ActivateCopyMode },
+  { key = "v",          mods = "CTRL",       action = act.PasteFrom "Clipboard" },
 }
 
 return config
