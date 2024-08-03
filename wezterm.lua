5c5bf6b82f9b445cc929a83842bdc7efc2c77058
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-08-03 19:28:47 +0900

    add keymaps to control panes

--- wezterm.lua
+++ wezterm.lua
@@ -1,9 +1,14 @@
+local function for_each(table, callback)
+  for i, v in ipairs(table) do
+    callback(v, i)
+  end
+end
+
 local wezterm = require "wezterm"
 
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
-
 
 config.window_decorations = "RESIZE"
 config.default_prog = { "wsl", "~" }
@@ -33,10 +38,53 @@ wezterm.on("gui-startup", function()
   window:gui_window():maximize()
 end)
 
+local secondary_pane_id = nil
 local act = wezterm.action
 config.keys = {
   -- new tab
   { key = "t",          mods = "SHIFT|CTRL", action = act.SpawnTab "CurrentPaneDomain" },
+  -- panes
+  { key = "LeftArrow",  mods = "CTRL|ALT",   action = act.ActivatePaneDirection "Left" },
+  { key = "RightArrow", mods = "CTRL|ALT",   action = act.ActivatePaneDirection "Right" },
+  { key = "UpArrow",    mods = "CTRL|ALT",   action = act.ActivatePaneDirection "Up" },
+  { key = "DownArrow",  mods = "CTRL|ALT",   action = act.ActivatePaneDirection "Down" },
+  { key = "d",          mods = "CTRL|SHIFT", action = act.CloseCurrentPane { confirm = true } },
+  {
+    key = "r",
+    mods = "CTRL|SHIFT|ALT",
+    action = wezterm.action_callback(function(window, pane)
+      local current_tab = pane:tab()
+      local panes = current_tab:panes_with_info()
+
+      main_pane = panes[1]
+      secondary_pane = nil
+      for_each(panes, function(p, i)
+        local pane_id = p.pane:pane_id()
+        if pane_id == secondary_pane_id then
+          secondary_pane = p
+        end
+      end)
+
+      -- if secondary_pane doesn't exist yet, make it and register it's id
+      if secondary_pane == nil then
+        pane:split {
+          direction = "Right",
+          size = 0.4,
+        }
+
+        local _panes = current_tab:panes_with_info()
+        secondary_pane_id = _panes[#_panes].pane:pane_id()
+        -- if main_pane isn't zoomed, activate and zoom it (in order to hide secondary_pane)
+      elseif not main_pane.is_zoomed then
+        main_pane.pane:activate()
+        current_tab:set_zoomed(true)
+        -- if main_pane is zoomed, quit zoom mode and activate secondary_pane
+      elseif main_pane.is_zoomed then
+        current_tab:set_zoomed(false)
+        secondary_pane.pane:activate()
+      end
+    end)
+  },
   -- cursor movements
   { key = "LeftArrow",  mods = "SHIFT|CTRL", action = act.SendKey { key = "b", mods = "META", } },
   { key = "RightArrow", mods = "CTRL",       action = act.SendKey { key = "f", mods = "META", } },
